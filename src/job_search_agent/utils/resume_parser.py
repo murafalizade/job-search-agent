fa478da4bdb40ce953752686322af3d96f3d5c24
murad.eliyev <murad.eliyev@cybernet.az>
2026-01-06 17:59:58 +0400

    feat(api): implement API endpoint for processing CVs with File upload support

--- src/job_search_agent/utils/resume_parser.py
+++ src/job_search_agent/utils/resume_parser.py
@@ -1,8 +1,9 @@
+import io
 import pdfplumber
 
-def resume_parser(path: str) -> str:
+def resume_parser(pdf_bytes: bytes) -> str:
     text = []
-    with pdfplumber.open(path) as pdf:
+    with pdfplumber.open(io.BytesIO(pdf_bytes)) as pdf:
         for page in pdf.pages:
             page_text = page.extract_text()
             if page_text:
